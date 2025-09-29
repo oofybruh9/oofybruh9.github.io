b910ca4fbf4e687071b65f5d887a68cdb8ec8263
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2025-09-28 17:33:55 -0700

    Update guac_install.sh to fix tomcat pkg
    tomcat9 not avaliable in alpine, changing to wiki guide to install tomcat

--- devcontainer-scripts/guac_install.sh
+++ devcontainer-scripts/guac_install.sh
@@ -2,20 +2,22 @@
 set -e
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.22/community" >> /etc/apk/repositories
 apk update
-apk add --no-cache guacamole-server guacamole-server-vnc guacamole-server-ssh guacamole-server-libs guacamole-server-encoder tomcat9
-wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /var/lib/tomcat9/webapps/guacamole.war
+apk add --no-cache guacamole-server guacamole-server-vnc guacamole-server-ssh guacamole-server-libs guacamole-server-encoder openjdk11-jre-headless wget
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.11/bin/apache-tomcat-11.0.11.tar.gz
+tar xvzf apache-tomcat-11.0.11.tar.gz --strip-components 1 --directory /opt/tomcat9
+wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /opt/tomcat9/webapps/guacamole.war
 mkdir /etc/guacamole
 cat << EOL >> /etc/guacamole/guacamole.properties
 guacd-hostname: localhost
 guacd-port: 4822
 
 EOL
-mkdir -p /usr/share/tomcat9/.guacamole
-ln -s /etc/guacamole /usr/share/tomcat9/.guacamole
+mkdir -p /opt/tomcat9/.guacamole
+ln -s /etc/guacamole /opt/.guacamole
 cat << EOIDK >> /usr/local/share/start-vnc.sh
 vncserver :1 &
 guacd -f
-/usr/share/tomcat9/bin/catalina.sh start
+/opt/tomcat9/bin/catalina.sh start
 
 EOIDK
 echo "done with everything!"
