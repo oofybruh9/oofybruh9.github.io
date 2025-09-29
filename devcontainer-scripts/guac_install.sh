987917e05f7179ce78ff439c1e0004f33a774061
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2025-09-28 18:14:16 -0700

    destroy guac cfg

--- devcontainer-scripts/guac_install.sh
+++ devcontainer-scripts/guac_install.sh
@@ -7,18 +7,9 @@ wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.11/bin/apache-tomcat-11.0.1
 mkdir /opt/tomcat9
 tar xvzf apache-tomcat-11.0.11.tar.gz --strip-components 1 --directory /opt/tomcat9
 wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /opt/tomcat9/webapps/guacamole.war
-mkdir /etc/guacamole
-cat << EOL >> /etc/guacamole/guacamole.properties
-guacd-hostname: localhost
-guacd-port: 4822
-
-EOL
-mkdir -p /opt/tomcat9/.guacamole
-ln -s /etc/guacamole /opt/.guacamole
 cat << EOIDK >> /usr/local/share/start-vnc.sh
 vncserver :1 &
 guacd -f
 /opt/tomcat9/bin/catalina.sh start
 
 EOIDK
-echo "done with everything!"
