5424db341af8f9964bb49db63f2c702c6634bd16
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2025-09-28 17:37:17 -0700

    Update guac_install.sh

--- devcontainer-scripts/guac_install.sh
+++ devcontainer-scripts/guac_install.sh
@@ -4,6 +4,7 @@ echo "http://dl-cdn.alpinelinux.org/alpine/v3.22/community" >> /etc/apk/reposito
 apk update
 apk add --no-cache guacamole-server guacamole-server-vnc guacamole-server-ssh guacamole-server-libs guacamole-server-encoder openjdk11-jre-headless wget
 wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.11/bin/apache-tomcat-11.0.11.tar.gz
+mkdir /opt/tomcat9
 tar xvzf apache-tomcat-11.0.11.tar.gz --strip-components 1 --directory /opt/tomcat9
 wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /opt/tomcat9/webapps/guacamole.war
 mkdir /etc/guacamole
