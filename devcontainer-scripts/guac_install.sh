0cff2fd440fdbba542ab447d311b2a2af532191b
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2025-09-28 18:21:05 -0700

    forget shebang

--- devcontainer-scripts/guac_install.sh
+++ devcontainer-scripts/guac_install.sh
@@ -8,6 +8,7 @@ mkdir /opt/tomcat9
 tar xvzf apache-tomcat-11.0.11.tar.gz --strip-components 1 --directory /opt/tomcat9
 wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /opt/tomcat9/webapps/guacamole.war
 cat << EOIDK >> /usr/local/share/start-vnc.sh
+#! /bin/bash
 vncserver :1 &
 guacd -f
 /opt/tomcat9/bin/catalina.sh start
