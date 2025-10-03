6df388b0a188e49af288b293b8c9242a264b46ff
oofybruh9 <122695223+oofybruh9@users.noreply.github.com>
2025-10-03 08:56:17 -0700

    Update pygame-dc.sh

--- devcontainer-scripts/pygame-dc.sh
+++ devcontainer-scripts/pygame-dc.sh
@@ -2,7 +2,8 @@
 set -e
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.22/community" >> /etc/apk/repositories
 apk update
-apk add --no-cache guacamole-server guacamole-server-vnc guacamole-server-ssh guacamole-server-libs guacamole-server-encoder openjdk11-jre-headless wget python3 python3-dev py3-pip python3-venv icewm
+setup-xorg-base
+apk add --no-cache guacamole-server guacamole-server-vnc guacamole-server-ssh guacamole-server-libs guacamole-server-encoder openjdk11-jre-headless wget python3 python3-dev py3-pip python3-venv icewm xterm
 ln -sf /usr/bin/python3 /usr/bin/python
 wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.11/bin/apache-tomcat-11.0.11.tar.gz
 mkdir /opt/tomcat9
@@ -10,6 +11,7 @@ tar xvzf apache-tomcat-11.0.11.tar.gz --strip-components 1 --directory /opt/tomc
 wget https://dlcdn.apache.org/guacamole/1.5.5/binary/guacamole-1.5.5.war -O /opt/tomcat9/webapps/guacamole.war
 cat << EOIDK >> /usr/local/share/start-vnc.sh
 #! /bin/sh
+startx icewm-session
 vncserver :1 &
 guacd -f &
 /opt/tomcat9/bin/catalina.sh start  &
